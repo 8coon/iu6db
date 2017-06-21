9e2d2bddf5143ea46d384c9c0b408b6d86371763
Kaibito <gpuzzletime@icloud.com>
2017-06-21 18:55:46 +0300

    Added npm autoinstall

--- frontend/start.js
+++ frontend/start.js
@@ -1,12 +1,26 @@
 'use strict';
 
 
-const execSync = require('child_process').execSync;
+const _execSync = require('child_process').execSync;
 const fs = require('fs');
+
+const execSync = (cmd, a) => {
+    try {
+        return _execSync(cmd, a);
+    } catch (e) {
+        if (e.status !== 0) {
+            process.exit(0);
+        }
+    }
+};
 
 
 process.chdir('./frontend');
 
+
+if (!fs.existsSync('./node_modules/')) {
+    execSync('npm install');
+}
 
 execSync('rm -rf ./dist');
 execSync('node ./node_modules/typescript/bin/tsc', {stdio: [0, 1, 2]});
